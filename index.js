c2649348d5b94a09c2765110ed3a229f5248cca0
connor dillon <connordillon96@gmail.com>
2020-12-05 17:25:10 +0000

    lang
    lang

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const chalkAnimation = require('chalk-animation');
 const franc = require('franc');
+const langs = require('langs');
 const input = process.argv.splice(2, process.argv.length - 1).join(' ');
+
 chalkAnimation.rainbow("Your input: " + input+ "\nwe think this is ->");
+
 setTimeout(() => {
-    console.log(franc(input));
-}, 1000);
+    if(franc(input) === 'und'){
+        chalkAnimation.pulse('your input was not recognised');
+    }
+    else{
+        const langCode = franc(input);
+        const language = langs.where("3",langCode);
+        console.log(language.name);
+    }
+}, 2000);
 
