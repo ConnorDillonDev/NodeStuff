b86452726adf4a84cc562188f7c76663ffd3b968
connor dillon <connordillon96@gmail.com>
2020-12-06 12:46:20 +0000

    as

--- express-template/index.js
+++ express-template/index.js
@@ -5,6 +5,15 @@ const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.get('/cats', (req, res)=>{
+    const cats = [
+        'Monty',
+        'Rocket',
+        'Winston'
+    ]
+    res.render('cats', {cats})
+})
+
 app.get('/', (req , res) =>{
     res.render('home')
 })
@@ -12,6 +21,11 @@ app.get('/', (req , res) =>{
 app.get('/random', (req, res) => {
     const num =  Math.floor(Math.random()*10) +1;
     res.render('random', {rand: num});
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', {subreddit})
 })
 
 app.listen(3000,() =>{
