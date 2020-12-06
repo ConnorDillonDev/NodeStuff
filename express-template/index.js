c50540efe053694b71755ace811bede78a106849
connor dillon <connordillon96@gmail.com>
2020-12-06 14:09:08 +0000

    partials

--- express-template/index.js
+++ express-template/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'))
+
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
+
+
 
 app.get('/cats', (req, res)=>{
     const cats = [
